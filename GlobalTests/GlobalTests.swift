c0aee23bb62894fe3c82a02bec40bf3c407fd99f
akshay Avati <akshay.avati@sspl-mac-10.local>
2019-02-11 11:03:01 +0530

    Initial Commit

---
+++ GlobalTests/GlobalTests.swift
@@ -0,0 +1,34 @@
+//
+//  GlobalTests.swift
+//  GlobalTests
+//
+//  Created by akshay Avati on 11/02/19.
+//  Copyright © 2019 akshay Avati. All rights reserved.
+//
+
+import XCTest
+@testable import Global
+
+class GlobalTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
